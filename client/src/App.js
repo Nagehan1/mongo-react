232d40c408345fe05b6d08bfd6447502a38ba117
Nagehan Uyumaz <100793893+Nagehan1@users.noreply.github.com>
2023-04-13 00:25:12 +0100

    router is ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,46 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import Register from "./pages/Register";
-import "./App.css";
 import Login from "./pages/Login";
+import Write from "./pages/Write";
 import Home from "./pages/Home";
 import Single from "./pages/Single";
-import Write from "./pages/Write";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: 
-      <div>
-        <Navbar />
-        <Home />
-        <Footer />
-      </div>
-    
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
     path: "/register",
@@ -29,9 +53,13 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <div>
-    <RouterProvider router={router} />
-  </div>;
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+    </div>
+  );
 }
 
 export default App;

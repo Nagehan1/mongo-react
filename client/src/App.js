021f1accd88d23a816746dadb14409c8545c3b7d
Nagehan Uyumaz <100793893+Nagehan1@users.noreply.github.com>
2023-04-12 23:47:12 +0100

    companents are ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,37 @@
-
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Register from "./pages/Register";
+import "./App.css";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+      <div>
+        <Navbar />
+        <Home />
+        <Footer />
+      </div>
+    
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
-  return (
-    <div>
-      hello
-    </div>
-  );
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
 }
 
 export default App;
